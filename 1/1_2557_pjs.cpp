b03960c1c29c3daee3862aece16bbd79b40b0b0b
korJiusngPark <0831park@gmail.com>
2018-05-31 14:01:18 +0900

    Add files via upload

---
+++ 1/1_2557_pjs.cpp
@@ -0,0 +1,11 @@
+
+#include "1_InputOutput_1.h"
+
+using namespace std;
+
+
+void print()
+{
+	cout << "Hello World!" << endl;
+}
+
---
+++ 1/6_10718_pjs.cpp
@@ -0,0 +1,9 @@
+#include "1.h"
+
+using namespace std;
+
+void print10718() {
+	cout << "강한친구 대한육군" << endl;
+	cout << "강한친구 대한육군" << endl;
+
+}
