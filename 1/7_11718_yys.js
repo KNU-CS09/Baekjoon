71fc2d01e81d5253a35073ce05786752e733c8dd
YoungSeok <alvin@madsq.net>
2018-06-08 21:26:57 +0900

    step 1 1~8 delete semicolon

--- 1/7_11718_yys.js
+++ 1/7_11718_yys.js
@@ -1,5 +1,5 @@
-var fs = require('fs');
-var input = fs.readFileSync('/dev/stdin').toString().split('\n');
+var fs = require('fs')
+var input = fs.readFileSync('/dev/stdin').toString().split('\n')
 
 for(var i in input){
   console.log(input[i])
