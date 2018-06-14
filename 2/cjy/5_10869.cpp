9ffbfdf21d0fa8c1a1f6fa58688472db7667bd8d
CHOI-DESKTOP\cjy9306 <richson1@naver.com>
2018-06-14 09:50:41 +0900

    step 2 commit

---
+++ 2/cjy/5_10869.cpp
@@ -0,0 +1,15 @@
+#include <stdio.h>
+/*
+int main()
+{
+	int a, b;
+
+	scanf("%d %d", &a, &b);
+
+	//A+B, A-B, A*B, A/B(¸ò), A%B(³ª¸ÓÁö)
+	printf("%d\n%d\n%d\n%d\n%d", (a + b), (a - b), (a*b), (a / b), (a%b));
+
+
+	return 0;
+}
+*/
