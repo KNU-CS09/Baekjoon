99f9552073b1a9243cb41a0ded10048b5adce0cf
K.Vwan <dopingcat@nate.com>
2018-06-14 10:50:25 +0900

    space to tab

--- 1/kdh/2_1000.py
+++ 1/kdh/2_1000.py
@@ -1,9 +1,10 @@
 import sys
+
 for line in sys.stdin:
-    parsedInput = line.split()
+	parsedInput = line.split()
 
-    a = int(parsedInput[0])
-    b = int(parsedInput[1])
+	a = int(parsedInput[0])
+	b = int(parsedInput[1])
 
-    if(a > 0 and b < 10):
-        print(a + b)
+	if(a > 0 and b < 10):
+		print(a + b)
--- 1/kdh/3_1001.py
+++ 1/kdh/3_1001.py
@@ -1,9 +1,10 @@
 import sys
+
 for line in sys.stdin:
-    parsedInput = line.split()
+	parsedInput = line.split()
 
-    a = int(parsedInput[0])
-    b = int(parsedInput[1])
+	a = int(parsedInput[0])
+	b = int(parsedInput[1])
 
-    if(0 < a < 10 and 0 < b < 10):
-        print(a - b)
+	if(0 < a < 10 and 0 < b < 10):
+		print(a - b)
